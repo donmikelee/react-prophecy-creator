1f7e655ac081d0ce05d26ececb393298baadc3a9
donmikelee <umpex94@gmail.com>
2021-06-11 14:58:28 +0200

    Add project

---
+++ build/app.js
@@ -0,0 +1,63 @@
+const Prophecy = (props) => <p>{props.prophecy}</p>;
+
+class App extends React.Component {
+  state = {
+    prophecies: ['wróżba I', 'wróżba II', 'wróżba III'],
+    drawnProphecy: '',
+    newProphecy: '',
+  };
+
+  handleProphecy = () => {
+    // debugger
+
+    const { prophecies } = this.state;
+
+    const min = 0;
+    const max = prophecies.length - 1;
+    const random = (min + Math.random() * (max - min)).toFixed(0);
+
+    const drawnProphecy = prophecies[random];
+
+    this.setState({
+      drawnProphecy: drawnProphecy,
+    });
+
+    console.log(drawnProphecy);
+  };
+
+  handleNewProphecy = (e) => {
+    this.setState({
+      newProphecy: e.target.value,
+    });
+  };
+
+  handleAddNewProphecy = () => {
+    // debugger
+
+    const { prophecies, newProphecy } = this.state;
+
+    const newPropheciesArray = prophecies.concat(newProphecy);
+
+    this.setState({
+      prophecies: newPropheciesArray,
+    });
+
+    alert(
+      `Właśnie dodałeś nową wróżbę ${newProphecy}! Twoje obecne wróżby to ${newPropheciesArray}.`
+    );
+  };
+
+  render() {
+    const { drawnProphecy } = this.state;
+
+    return (
+      <>
+        <button onClick={this.handleProphecy}>Zobacz wróżbę</button>
+        <input onChange={this.handleNewProphecy}></input>
+        <button onClick={this.handleAddNewProphecy}>Dodaj wróżbę</button>
+        <Prophecy prophecy={drawnProphecy} />
+      </>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
